e8c75aa5d126f3a2e336733df738558123a7d9e2
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-02-24 17:24:48 +0100

    flipcards tabbaar #8 #2
    flipcards draaien om als je over hun heen tabt

--- public/script.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector("button")
-const kaartje = document.querySelector(".kaartje")
-
-button.addEventListener('click', spin)
-
-function spin() {
-  kaartje.classList.toggle('hover')
-}
